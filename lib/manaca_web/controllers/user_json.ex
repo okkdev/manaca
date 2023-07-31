016cb19cee29f55655ec810f85a3f40b6ec24e72
okkdev <dev@stehlik.me>
2023-07-31 10:37:08 +0200

    gen api

--- lib/manaca_web/controllers/user_json.ex
+++ lib/manaca_web/controllers/user_json.ex
@@ -1,2 +1,27 @@
 defmodule ManacaWeb.UserJson do
+  alias Manaca.Accounts.User
+
+  @doc """
+  Renders a list of users.
+  """
+  def index(%{users: users}) do
+    %{data: for(user <- users, do: data(user))}
+  end
+
+  @doc """
+  Renders a single user.
+  """
+  def show(%{user: user}) do
+    %{data: data(user)}
+  end
+
+  defp data(%User{} = user) do
+    %{
+      id: user.id,
+      tokens: user.tokens,
+      firstname: user.firstname,
+      lastname: user.lastname,
+      email: user.email
+    }
+  end
 end
